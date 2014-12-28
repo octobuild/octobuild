bee37f3e842405a06201e8be9832c19ead7ee554
Artem V. Navrotskiy <bozaro@users.noreply.github.com>
2014-12-28 22:56:40 +0300

    Show command line arguments in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-﻿fn main() {
-    println!("Hello, world!");
+use std::os;
+
+fn main() {
+	for arg in parse_command_line(os::args()).iter() {
+		println!("{}", arg);
+	}
+}
+
+fn parse_command_line(args: Vec<String>) -> Vec<String> {
+	let mut result: Vec<String> = Vec::new();
+	for arg in args.slice(1, args.len()).iter() {
+		result.push(arg.clone());
+	}
+	result
 }
