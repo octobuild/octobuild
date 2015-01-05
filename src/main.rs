e248c698cec99fd76b1047802c9a5b4acf6d1860
Artem V. Navrotskiy <bozaro@yandex.ru>
2015-01-05 09:15:13 +0300

    Minimal tools and tasks parsing.

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,6 @@ use std::fmt;
 
 use xml::reader::EventReader;
 use xml::reader::events::XmlEvent;
-use xml::reader::events::XmlEvent::{StartElement, EndElement, Error};
-
-fn indent(size: uint) -> String {
-	let mut result = String::with_capacity(size*4);
-	for _ in range(0, size) {
-			result.push_str("    ");
-	}
-	result
-}
 
 fn main() {
 	println!("XGConsole:");
@@ -33,14 +24,47 @@ fn parse_command_line(args: Vec<String>) -> Vec<String> {
 	result
 }
 
-struct IbTask {
-name: String,
-dependsOn: Vec<String>
+enum OptionalString {
+Value(String),
+Missing,
 }
 
-impl fmt::Show for IbTask {
+impl fmt::Show for OptionalString {
 fn fmt(& self, f: &mut fmt::Formatter) -> fmt::Result {
-	write!(f, "name={}, dependsOn={}", self .name, self .dependsOn)
+	match self {
+			&OptionalString::Value(ref v) => {
+			write!(f, "{}", v)
+		}
+			&OptionalString::Missing => {
+			write!(f, "missing")
+		}
+		}
+}
+}
+
+struct XgTask {
+id: OptionalString,
+title: OptionalString,
+tool: OptionalString,
+working_dir: OptionalString,
+depends_on: Vec<String>,
+}
+
+impl fmt::Show for XgTask {
+fn fmt(& self, f: &mut fmt::Formatter) -> fmt::Result {
+	write!(f, "id={}, title={}, tool={}, working_dir={}, depends_on={}", self .id, self .title, self .tool, self .working_dir, self .depends_on)
+}
+}
+
+struct XgTool {
+id: OptionalString,
+path: OptionalString,
+params: OptionalString,
+}
+
+impl fmt::Show for XgTool {
+fn fmt(& self, f: &mut fmt::Formatter) -> fmt::Result {
+	write!(f, "id={}, path={}", self .id, self .path)
 }
 }
 
@@ -49,51 +73,96 @@ fn sample() {
 	let reader = BufferedReader::new(file);
 
 	let mut parser = EventReader::new(reader);
-	let mut depth = 0;
+	let mut tasks:Vec<XgTask> = vec![];
+	let mut tools:Vec<XgTool> = vec![];
 	for e in parser.events() {
 		match e {
-				StartElement { name, attributes, namespace} => {
+				XmlEvent::StartElement {name, attributes, namespace} => {
 				match name.local_name.as_slice() {
 						"Task" =>
 						{
-							let mut task = IbTask {
-							name: "".to_string(),
-							dependsOn: vec![]
-							};
-							println!("{}/{} {}!", indent(depth), name, attributes);
-							for attr in attributes.iter() {
-								//let a : int = attr.value;
-								match attr.name.local_name.as_slice()
-									{
-										"Name" =>
-										{
-												task.name = attr.value.to_string();
-										}
-										"DependsOn" =>
-										{
-											for item in attr.value.split_str(";").collect::<Vec<&str>>().iter() {
-												println!(" deps: {}", item);
-												task.dependsOn.push(item.to_string());
-											}
-										}
-										_ =>
-										{}
-									}
-								println!(" attr: {}", attr);
-								println!(" task: {}", task);
-							}
+								tasks.push(xg_parse_task(&attributes));
+						}
+						"Tool" =>
+						{
+								tools.push(xg_parse_tool(&attributes));
 						}
 						_ => {}
 					}
-				println!("{}/{}", indent(depth), name);
-				depth += 1;
 			}
-				EndElement{name} => {
-				depth -= 1;
-				println!("{}/{}", indent(depth), name);
+				XmlEvent::EndElement{name} => {
 			}
 				_ => {
 			}
 			}
 	}
+}
+
+fn xg_parse_task (attributes: &Vec<xml::attribute::OwnedAttribute>)->XgTask {
+	let mut task = XgTask {
+	id: OptionalString::Missing,
+	title: OptionalString::Missing,
+	tool: OptionalString::Missing,
+	working_dir: OptionalString::Missing,
+	depends_on: vec![],
+	};
+	for attr in attributes.iter() {
+		match attr.name.local_name.as_slice()
+			{
+				"Name" =>
+				{
+						task.id = OptionalString::Value(attr.value.to_string());
+				}
+				"Caption" =>
+				{
+						task.title = OptionalString::Value(attr.value.to_string());
+				}
+				"Tool" =>
+				{
+						task.tool = OptionalString::Value(attr.value.to_string());
+				}
+				"WorkingDir" =>
+				{
+						task.working_dir = OptionalString::Value(attr.value.to_string());
+				}
+				"DependsOn" =>
+				{
+					for item in attr.value.split_str(";").collect::<Vec<&str>>().iter() {
+							task.depends_on.push(item.to_string());
+					}
+				}
+				_ =>
+				{
+				}
+			}
+	}
+	task
+}
+fn xg_parse_tool (attributes: &Vec<xml::attribute::OwnedAttribute>)->XgTool {
+	let mut tool = XgTool {
+	id: OptionalString::Missing,
+	path: OptionalString::Missing,
+	params: OptionalString::Missing,
+	};
+	for attr in attributes.iter() {
+		match attr.name.local_name.as_slice()
+			{
+				"Name" =>
+				{
+						tool.id = OptionalString::Value(attr.value.to_string());
+				}
+				"Path" =>
+				{
+						tool.path = OptionalString::Value(attr.value.to_string());
+				}
+				"Params" =>
+				{
+						tool.params = OptionalString::Value(attr.value.to_string());
+				}
+				_ =>
+				{
+				}
+			}
+	}
+	tool
 }
