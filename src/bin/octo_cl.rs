eaad5ca4852c87d97a5625f510b98680d091fe22
Artem V. Navrotskiy <bozaro@yandex.ru>
2015-01-23 14:41:08 +0300

    Move compilation code to module.

--- src/bin/octo_cl.rs
+++ src/bin/octo_cl.rs
@@ -1,18 +1,12 @@
 #![allow(unstable)]
 extern crate octobuild;
 extern crate log;
-extern crate "sha1-hasher" as sha1;
 
-use octobuild::wincmd;
-use octobuild::cache::Cache;
-use octobuild::io::tempfile::TempFile;
 use octobuild::vs::compiler::VsCompiler;
 use octobuild::compiler::Compiler;
-use octobuild::compiler::{Arg, Scope, CompilationTask, PreprocessResult};
-use octobuild::utils::{filter, hash_sha1};
-use std::os;
 
-use std::io::{Command, File, IoError, TempDir};
+use std::os;
+use std::io::TempDir;
 
 fn main() {
 	let temp_dir = match TempDir::new("octobuild") {
@@ -20,13 +14,14 @@ fn main() {
 		Err(e) => {panic!(e);}
 	};
 	let compiler = VsCompiler::new(temp_dir.path());
+
 	let result = compiler.create_task(&os::args()[1..]);
 	println!("Parsed task: {:?}", result);
 	match result {
 			Ok(task) => {
 				match compiler.preprocess(&task) {
 					Ok(result) => {
-						compile(&temp_dir, &task, result);
+						compiler.compile(&task, result);
 					}
 					Err(e) => {
 							panic!(e);
@@ -47,95 +42,4 @@ fn main() {
 			panic!("{}", e);
 		}
 		}*/
-}
-
-
-
-fn compile(temp_dir: &TempDir, task: &CompilationTask, preprocessed: PreprocessResult) -> Result<(), IoError> {
-	let cache: Cache = Cache::new();
-	let mut args = filter(&task.args, |arg:&Arg|->Option<String> {
-		match arg {
-			&Arg::Flag{ref scope, ref flag} => {
-				match scope {
-					&Scope::Preprocessor | &Scope::Compiler | &Scope::Shared => Some("/".to_string() + flag.as_slice()),
-					&Scope::Ignore => None
-				}
-			}
-			&Arg::Param{ref scope, ref  flag, ref value} => {
-				match scope {
-					&Scope::Preprocessor | &Scope::Compiler | &Scope::Shared => Some("/".to_string() + flag.as_slice() + value.as_slice()),
-					&Scope::Ignore => None
-				}
-			}
-			&Arg::Input{..} => None,
-			&Arg::Output{..} => None
-		}
-	});
-	args.push("/T".to_string() + task.language.as_slice());
-	match &task.input_precompiled {
-		&Some(ref path) => {
-			args.push("/Yu".to_string());
-			args.push("/Fp".to_string() + path.display().to_string().as_slice());
-		}
-		&None => {}
-	}
-	if task.output_precompiled.is_some() {
-		args.push("/Yc".to_string());
-	}
-	// Input data, stored in files.
-	let mut inputs: Vec<Path> = Vec::new();
-	match &task.input_precompiled {
-			&Some(ref path) => {inputs.push(path.clone());}
-			&None => {}
-		}
-	// Output files.
-	let mut outputs: Vec<Path> = Vec::new();
-	outputs.push(task.output_object.clone());
-	match &task.output_precompiled {
-		&Some(ref path) => {outputs.push(path.clone());}
-		&None => {}
-	}
-
-	let hash_params = hash_sha1(preprocessed.content.as_slice()) + wincmd::join(&args).as_slice();
-	cache.run_cached(hash_params.as_slice(), &inputs, &outputs, || -> Result<(), IoError> {
-		// Input file path.
-		let input_temp = TempFile::new_in(temp_dir.path(), ".i");
-		try! (File::create(input_temp.path()).write(preprocessed.content.as_slice()));
-		// Run compiler.
-		let mut command = Command::new("cl.exe");
-		command
-			.args(args.as_slice())
-			.arg(input_temp.path().display().to_string())
-			.arg("/c".to_string())
-			.arg("/Fo".to_string() + task.output_object.display().to_string().as_slice());
-		match &task.input_precompiled {
-			&Some(ref path) => {command.arg("/Fp".to_string() + path.display().to_string().as_slice());}
-			&None => {}
-		}
-		match &task.output_precompiled {
-			&Some(ref path) => {command.arg("/Fp".to_string() + path.display().to_string().as_slice());}
-			&None => {}
-		}
-	
-		let output = try! (command.output());
-		println!("stdout: {}", String::from_utf8_lossy(output.output.as_slice()));
-		println!("stderr: {}", String::from_utf8_lossy(output.error.as_slice()));
-
-		Ok(())
-	})
-}
-
-#[test]
-fn test_precompiled_header()  {
-		wincmd::parse("/c /Ycsample.h /Fpsample.h.pch /Foprecompiled.cpp.o precompiled.cpp");
-}
-
-#[test]
-fn test_compile_no_header()   {
-		wincmd::parse("/c /Fosample.cpp.o sample.cpp");
-}
-
-#[test]
-fn test_compile_with_header() {
-		wincmd::parse("/c /Yusample.h /Fpsample.h.pch /Fosample.cpp.o sample.cpp");
 }
--- src/vs/compiler.rs
+++ src/vs/compiler.rs
@@ -3,20 +3,24 @@ extern crate "sha1-hasher" as sha1;
 pub use super::super::compiler::Compiler;
 pub use super::super::compiler::{Arg, CompilationTask, PreprocessResult, Scope};
 
+use super::super::cache::Cache;
 use super::postprocess;
 use super::super::wincmd;
 use super::super::utils::filter;
+use super::super::utils::hash_sha1;
 use super::super::io::tempfile::TempFile;
 
 use std::io::{Command, File, IoError, IoErrorKind};
 
 pub struct VsCompiler {
+	cache: Cache,
 	temp_dir: Path
 }
 
 impl VsCompiler {
 	pub fn new(temp_dir: &Path) -> Self {
 		VsCompiler {
+			cache: Cache::new(),
 			temp_dir: temp_dir.clone()
 		}
 	}
@@ -105,6 +109,74 @@ impl Compiler for VsCompiler {
 
 	// Compile preprocessed file.
 	fn compile(&self, task: &CompilationTask, preprocessed: PreprocessResult) -> Result<(), IoError> {
-		Ok(())
+		let mut args = filter(&task.args, |arg:&Arg|->Option<String> {
+			match arg {
+				&Arg::Flag{ref scope, ref flag} => {
+					match scope {
+						&Scope::Preprocessor | &Scope::Compiler | &Scope::Shared => Some("/".to_string() + flag.as_slice()),
+						&Scope::Ignore => None
+					}
+				}
+				&Arg::Param{ref scope, ref  flag, ref value} => {
+					match scope {
+						&Scope::Preprocessor | &Scope::Compiler | &Scope::Shared => Some("/".to_string() + flag.as_slice() + value.as_slice()),
+						&Scope::Ignore => None
+					}
+				}
+				&Arg::Input{..} => None,
+				&Arg::Output{..} => None
+			}
+		});
+		args.push("/T".to_string() + task.language.as_slice());
+		match &task.input_precompiled {
+			&Some(ref path) => {
+				args.push("/Yu".to_string());
+				args.push("/Fp".to_string() + path.display().to_string().as_slice());
+			}
+			&None => {}
+		}
+		if task.output_precompiled.is_some() {
+			args.push("/Yc".to_string());
+		}
+		// Input data, stored in files.
+		let mut inputs: Vec<Path> = Vec::new();
+		match &task.input_precompiled {
+				&Some(ref path) => {inputs.push(path.clone());}
+				&None => {}
+			}
+		// Output files.
+		let mut outputs: Vec<Path> = Vec::new();
+		outputs.push(task.output_object.clone());
+		match &task.output_precompiled {
+			&Some(ref path) => {outputs.push(path.clone());}
+			&None => {}
+		}
+	
+		let hash_params = hash_sha1(preprocessed.content.as_slice()) + wincmd::join(&args).as_slice();
+		self.cache.run_cached(hash_params.as_slice(), &inputs, &outputs, || -> Result<(), IoError> {
+			// Input file path.
+			let input_temp = TempFile::new_in(&self.temp_dir, ".i");
+			try! (File::create(input_temp.path()).write(preprocessed.content.as_slice()));
+			// Run compiler.
+			let mut command = Command::new("cl.exe");
+			command
+				.args(args.as_slice())
+				.arg(input_temp.path().display().to_string())
+				.arg("/c".to_string())
+				.arg("/Fo".to_string() + task.output_object.display().to_string().as_slice());
+			match &task.input_precompiled {
+				&Some(ref path) => {command.arg("/Fp".to_string() + path.display().to_string().as_slice());}
+				&None => {}
+			}
+			match &task.output_precompiled {
+				&Some(ref path) => {command.arg("/Fp".to_string() + path.display().to_string().as_slice());}
+				&None => {}
+			}
+		
+			let output = try! (command.output());
+			println!("stdout: {}", String::from_utf8_lossy(output.output.as_slice()));
+			println!("stderr: {}", String::from_utf8_lossy(output.error.as_slice()));
+			Ok(())
+		})
 	}
 }
