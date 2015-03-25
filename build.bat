9b4004505065e60ff43d8f5dcdc52d70d0d3048e
Artem V. Navrotskiy <bozaro@yandex.ru>
2015-03-25 19:03:23 +0300

    WixSharp installer

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 set WIX_ARCH=x64
-cargo build --release && "%WIX%\bin\candle.exe" -arch %WIX_ARCH% octobuild.wix -o target\octobuild-%WIX_ARCH%.wixobj -nologo && "%WIX%\bin\light.exe" -sw1076 -ext WixUIExtension -o target\octobuild-%WIX_ARCH%.msi -b stage-%WIX_ARCH% -nologo target/octobuild-%WIX_ARCH%.wixobj
+cd %~dp0
+cargo build --release && %WIXSHARP_DIR%\cscs.exe wixcs\setup.cs InstallDir
---
+++ wixcs/setup.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Text.RegularExpressions;
+using WixSharp;
+
+class Script
+{
+	static public string ReadVersion(string path)
+	{
+		System.IO.StreamReader file = new System.IO.StreamReader(path);
+		Regex regex = new Regex("^\\s*version\\s*=\\s*\"(\\S+)\"");
+		string line;
+		while ((line = file.ReadLine()) != null)
+		{
+			Match match = regex.Match(line);
+			if (match.Success)
+			{
+				return match.Groups[1].Value;
+			}
+		}
+		return null;
+	}
+
+	static public void Main(string[] args)
+	{
+		String version = ReadVersion(@"Cargo.toml");
+		Project project = new Project("Octobuild",
+			new Dir(@"%ProgramFiles64Folder%\octobuild",
+				new File(@"target\release\xgconsole.exe"),
+				new File(@"LICENSE-MIT")
+			)
+		);
+		project.LicenceFile = @"LICENSE-MIT.rtf";
+		project.GUID = new Guid("b4505233-6377-406b-955b-2547d86a99a7");
+		project.UI = WUI.WixUI_InstallDir;
+		project.Package.AttributesDefinition = "Platform=x64";
+		project.Version = new Version(version);
+		project.OutFileName = @"target\octobuild-" + version + "-x86_64";
+		
+		foreach (var file in project.AllFiles)
+		{
+			file.Attributes.Add("Component:Win64", "yes");
+		}
+
+		Compiler.BuildMsi(project);
+	}
+}
