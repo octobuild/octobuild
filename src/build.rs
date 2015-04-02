c8b69ed4d22d5c10379a0e6b22b568a890eb5bb6
Artem V. Navrotskiy <bozaro@yandex.ru>
2015-04-02 10:43:08 +0300

    Add support for AppVeyor installer build

---
+++ src/build.rs
@@ -0,0 +1,12 @@
+﻿use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+
+fn main() {
+    let root_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let profile = env::var("PROFILE").unwrap();
+    let dest_path = Path::new(&root_dir).join("target").join(&profile).join("target.txt");
+    let mut f = File::create(&dest_path).unwrap();
+    f.write_all(env::var("TARGET").unwrap().as_bytes()).unwrap();
+}
