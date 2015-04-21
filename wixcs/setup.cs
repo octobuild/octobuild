ff8c8bb47e68a2802d2bbc5ca9c136c285620ff6
Artem V. Navrotskiy <bozaro@yandex.ru>
2015-04-21 23:17:14 +0300

    Add support for installation to unpriveleged user fixed #1

--- wixcs/setup.cs
+++ wixcs/setup.cs
@@ -8,55 +8,56 @@ using WixSharp;
 
 class Script
 {
-	enum Target
-	{
-		i686,
-		x86_64,
-	}
+    enum Target
+    {
+        i686,
+        x86_64,
+    }
 
-	static public string ReadVersion(string path)
-	{
-		System.IO.StreamReader file = new System.IO.StreamReader(path);
-		Regex regex = new Regex("^\\s*version\\s*=\\s*\"(\\S+)\"");
-		string line;
-		while ((line = file.ReadLine()) != null)
-		{
-			Match match = regex.Match(line);
-			if (match.Success)
-			{
-				return match.Groups[1].Value;
-			}
-		}
-		return null;
-	}
+    static public string ReadVersion(string path)
+    {
+        System.IO.StreamReader file = new System.IO.StreamReader(path);
+        Regex regex = new Regex("^\\s*version\\s*=\\s*\"(\\S+)\"");
+        string line;
+        while ((line = file.ReadLine()) != null)
+        {
+            Match match = regex.Match(line);
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+        }
+        return null;
+    }
 
-	static Target ReadTarget(string path)
-	{
-		String target = System.IO.File.ReadAllText(path);
-		switch (target)
-		{
-			case "x86_64-pc-windows-gnu":
-				return Target.x86_64;
-			case "i686-pc-windows-gnu":
-				return Target.i686;
-			default:
-				throw new Exception("Unknown target: " + target);
-		}
-	}
+    static Target ReadTarget(string path)
+    {
+        String target = System.IO.File.ReadAllText(path);
+        switch (target)
+        {
+            case "x86_64-pc-windows-gnu":
+                return Target.x86_64;
+            case "i686-pc-windows-gnu":
+                return Target.i686;
+            default:
+                throw new Exception("Unknown target: " + target);
+        }
+    }
 
-	static void CreateNuspec(string template, string output, string version, Target target)
-	{
-		string content = System.IO.File.ReadAllText(template, Encoding.UTF8);
-		content = content.Replace("$version$", version);
-		content = content.Replace("$target$", target.ToString());
-		System.IO.File.WriteAllText(output, content, Encoding.UTF8);
-	}
+    static void CreateNuspec(string template, string output, string version, Target target)
+    {
+        string content = System.IO.File.ReadAllText(template, Encoding.UTF8);
+        content = content.Replace("$version$", version);
+        content = content.Replace("$target$", target.ToString());
+        System.IO.File.WriteAllText(output, content, Encoding.UTF8);
+    }
 
     static public void Main(string[] args)
     {
         Target target = ReadTarget(@"target\release\target.txt");
         String version = ReadVersion(@"Cargo.toml");
         Feature featureBuilder = new Feature("Octobuild Builder", true, false);
+        featureBuilder.AttributesDefinition = @"AllowAdvertise=no";
         String programFile = (target == Target.x86_64) ? "%ProgramFiles64Folder%" : "%ProgramFilesFolder%";
 
         List<WixEntity> files = new List<WixEntity>();
@@ -67,26 +68,38 @@ class Script
             files.Add(new File(featureBuilder, file));
         }
         Project project = new Project("Octobuild",
-            new Dir(programFile + @"\Octobuild", files.ToArray()),
-            new EnvironmentVariable(featureBuilder, "PATH", "[INSTALLDIR]")
+            new Property("ApplicationFolderName", "Octobuild"),
+            new Property("WixAppFolder", "WixPerMachineFolder"),
+            new Dir(new Id("APPLICATIONFOLDER"), programFile + @"\Octobuild", files.ToArray()),
+            new EnvironmentVariable(featureBuilder, "PATH", "[APPLICATIONFOLDER]")
             {
                 Permanent = false,
                 Part = EnvVarPart.last,
                 Action = EnvVarAction.set,
                 System = true,
+                Condition = new Condition("ALLUSERS")
+            },
+            new EnvironmentVariable(featureBuilder, "PATH", "[APPLICATIONFOLDER]")
+            {
+                Permanent = false,
+                Part = EnvVarPart.last,
+                Action = EnvVarAction.set,
+                System = false,
+                Condition = new Condition("NOT ALLUSERS")
             }
         );
         project.LicenceFile = @"LICENSE.rtf";
         project.LicenceFile = @"LICENSE.rtf";
         project.GUID = new Guid("b4505233-6377-406b-955b-2547d86a99a7");
-        project.UI = WUI.WixUI_InstallDir;
+        project.UI = WUI.WixUI_Advanced;
         project.Version = new Version(version);
         project.Manufacturer = "Artem V. Navrotskiy";
         project.OutFileName = @"target\octobuild-" + version + "-" + target;
+        project.Package.AttributesDefinition = @"InstallPrivileges=elevated";
 
         if (target == Target.x86_64)
         {
-            project.Package.AttributesDefinition = @"Platform=x64;InstallScope=perMachine";
+            project.Package.AttributesDefinition += @";Platform=x64";
             Compiler.WixSourceGenerated += new XDocumentGeneratedDlgt(Compiler_WixSourceGenerated);
         }
 
@@ -95,11 +108,11 @@ class Script
         CreateNuspec(@"wixcs\octobuild.nuspec", @"target\octobuild.nuspec", version, target);
     }
 
-	static void Compiler_WixSourceGenerated(XDocument document)
-	{
-		foreach (XElement comp in document.Root.AllElements("Component"))
-		{
-			comp.Add(new XAttribute("Win64", "yes"));
-		}
-	}
+    static void Compiler_WixSourceGenerated(XDocument document)
+    {
+        foreach (XElement comp in document.Root.AllElements("Component"))
+        {
+            comp.Add(new XAttribute("Win64", "yes"));
+        }
+    }
 }
