9793caae3acfd9e255418cd462beb77f345446e1
Artem V. Navrotskiy <bozaro@yandex.ru>
2015-05-20 09:55:48 +0300

    lz4 update: compilation fix

--- src/cache.rs
+++ src/cache.rs
@@ -199,7 +199,7 @@ fn write_cache(path: &Path, paths: &Vec<PathBuf>, output: &OutputInfo) -> Result
 	}
 
 	fn write_cache_compressed<W: Write>(write: W, paths: &Vec<PathBuf>, output: &OutputInfo) -> Result<(), Error> {
-		let mut stream = try! (lz4::Encoder::new(write, 1));
+		let mut stream = try! (lz4::EncoderParams::new().level(1).build(write));
 		try! (write_cache_inner(&mut stream, paths, output));
 		match stream.finish() {
 			(_, result) => result
