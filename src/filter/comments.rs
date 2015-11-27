676631fbeb63f7b928c66c2a22aaa1153ebed2c6
Artem V. Navrotskiy <bozaro@users.noreply.github.com>
2015-11-27 15:18:40 +0300

    Fix multiline comments with multiline defines

--- src/filter/comments.rs
+++ src/filter/comments.rs
@@ -1,4 +1,4 @@
-use std::io::{Read, Error};
+use std::io::{Read, Error, ErrorKind};
 
 #[derive(Copy, Clone, Eq, PartialEq)]
 enum MultiLineMark {
@@ -10,11 +10,11 @@ enum MultiLineMark {
 }
 
 enum State {
-	Code(MultiLineMark),
-	Quote(u8),
+	Code(MultiLineMark, Option<u8>),
+	Quote(u8, Option<u8>),
 	Escape(u8),
 	SingleLineComment(MultiLineMark),
-	MultLineComment(MultiLineMark),
+	MultLineComment(MultiLineMark, bool, Option<u8>),
 }
 
 /**
@@ -22,16 +22,56 @@ enum State {
  */
 pub struct CommentsRemover<R> {
 	r: R,
+	// Parser state
 	state: State,
-	last: Option<u8>,
+	// Buffer with size/position
+	buffer: Vec<u8>,
+	offset: usize,
+	limit: usize,
 }
 
 impl<R: Read> CommentsRemover<R> {
 	pub fn new(r: R) -> CommentsRemover<R> {
+		CommentsRemover::new_with_buffer(r, 4096)
+	}
+
+	pub fn new_with_buffer(r: R, buffer_size: usize) -> CommentsRemover<R> {
+		assert!(buffer_size > 0);
 		CommentsRemover {
 			r: r,
-			state: State::Code(MultiLineMark::None),
-			last: None,
+			state: State::Code(MultiLineMark::None, None),
+			buffer: vec! [0; buffer_size],
+			offset: 0,
+			limit: 0,
+		}
+	}
+
+	fn preload(&mut self) -> Result<bool, Error> {
+		if self.offset == self.limit {
+			self.offset = 0;
+			self.limit = 0;
+		}
+		let buffer_size = self.buffer.len();
+		if self.limit < buffer_size {
+			let size = try! (self.r.read(&mut self.buffer[self.offset..buffer_size]));
+			self.limit += size;
+		}
+		Ok(self.limit > 0)
+	}
+
+	fn flush(&mut self, buf: &mut [u8], offset: usize) -> usize {
+		match self.state {
+			State::Escape(_) | State::SingleLineComment(_) | State::Quote(_, None) | State::Code(_, _) | State::MultLineComment(_, _, None) => offset,
+			State::MultLineComment(multiline, end, Some(c)) => {
+				self.state = State::MultLineComment(multiline, end, None);
+				buf[offset] = c;
+			 	offset + 1
+			}
+			State::Quote(q, Some(c)) => {
+				self.state = State::Quote(q, None);
+				buf[offset] = c;
+			 	offset + 1
+			}
 		}
 	}
 }
@@ -41,166 +81,141 @@ impl<R: Read> Read for CommentsRemover<R> {
 		if buf.len() == 0 {
 			return Ok(0);
 		}
-		let size = try! (self.r.read(buf));
-		let mut offset = 0;
-		for index in 0..size {	
-			let c = buf[index];
-			match self.state {
-				State::Code(multiline) => {
+		let mut index = 0;
+		while index < buf.len() {
+			index = self.flush(buf, index);
+			if index == buf.len() {
+				break;
+			}
+			assert!(index < buf.len());
+			if self.offset >= self.limit {
+				if try! (self.preload()) {
+					continue;
+				}
+				match self.state {
+					State::Code(_, None) => {
+						break;
+					}
+					State::Code(_, Some(last)) => {
+						buf[index] = last;
+						index += 1;
+						self.state = State::Code(MultiLineMark::None, None);
+						break;
+					}
+					_ => {
+						return Err(Error::new(ErrorKind::InvalidInput, "Unexpected end of data"));
+					}
+				}
+			}
+			let c = self.buffer[self.offset];
+			self.offset += 1;
+			self.state = match self.state {
+				State::Code(multiline, last) => {
 					match c {
 						b'"' | b'\'' => {
-							self.state = State::Quote(c);
-						}
-						b'/' if self.last == Some(b'/') => {
-							self.last = None;
-							self.state = State::SingleLineComment(multiline);
-						}
-						b'*' if self.last == Some(b'/') => {
-							self.last = None;
-							self.state = State::MultLineComment(multiline);
-						}
-						c => {
-							match self.last {
+							match last {
 								Some(last) => {
-									buf[offset] = last;
-									offset += 1;
+									buf[index] = last;
+									index += 1;
 								}
 								None => {
 								}
 							}
-							self.last = Some(c);
+							State::Quote(c, Some(c))
+						}
+						b'/' if last == Some(b'/') => State::SingleLineComment(multiline),
+						b'*' if last == Some(b'/') => State::MultLineComment(multiline, false, None),
+						c => {
+							match last {
+								Some(last) => {
+									buf[index] = last;
+									index += 1;
+								}
+								None => {
+								}
+							}
 							self.state = State::Code(match c {
 								b'/' => multiline,
 								b'\\' => MultiLineMark::Space,
 								b'\t' | b' ' if multiline == MultiLineMark::Space => MultiLineMark::Space,
-								b'\t' | b' ' => MultiLineMark::NewLine,
+								b'\t' | b' ' if multiline == MultiLineMark::None => MultiLineMark::None,
+								b'\t' | b' '  => MultiLineMark::NewLine,
 								b'\n' if multiline == MultiLineMark::Space => MultiLineMark::Lf,
 								b'\n' if multiline == MultiLineMark::Cr => MultiLineMark::Cr,
 								b'\r' if multiline == MultiLineMark::Space => MultiLineMark::Cr,
 								b'\r' if multiline == MultiLineMark::Lf => MultiLineMark::Lf,
 								_ => MultiLineMark::None,
-							});
+							}, Some(c));
 							continue;
 						}
-					};
+					}
 				}
 				State::Escape(q) => {
-					self.state = State::Quote(q);
+					State::Quote(q, Some(c))
 				}
-				State::Quote(q) => {
+				State::Quote(q, last) => {
+					assert!(last.is_none());
+					buf[index] = c;
+					index += 1;
 					match c {
-						b'\\' => {
-							self.state = State::Escape(q);
-						}
-						c if c == q => {
-							self.state = State::Code(MultiLineMark::None);
-						}
-						_ => {
-						}
+						b'\\' => State::Escape(q),
+						c if c == q => State::Code(MultiLineMark::None, None),
+						_ => State::Quote(q, None),
 					}
 				}
 				State::SingleLineComment(multiline) => {
 					match c {
-						b'\n' | b'\r' => {
-							self.state = State::Code(multiline);
-						}
-						_ => {
-						}
+						b'\n' | b'\r' => State::Code(multiline, Some(c)),
+						_ => State::SingleLineComment(multiline),
 					}
 				}
-				State::MultLineComment(multiline) => {
+				State::MultLineComment(multiline, end, last) => {
+					assert!(last.is_none());
 					match c {
-						b'/' if self.last == Some(b'*') => {
-							self.last = None;
-							self.state = State::Code(multiline);
-							continue;
-						}
-						v => {
-							self.last = Some(v);
-						}
-					}
-				}
-			}
-			match self.state {
-				State::Code(_) | State::Quote(_) | State::Escape(_) => {
-					match self.last {
-						Some(c) => {
-							buf[offset] = c;
-							offset += 1;
-						}
-						None => {
-						}
-					}
-					self.last = Some(c);
-				}
-				State::MultLineComment(multiline) => {
-					match c {
+						b'/' if end => State::Code(multiline, None),
+						b'*' => State::MultLineComment(multiline, true, None),
 						b'\n' | b'\r' => {
 							match multiline {
 								MultiLineMark::NewLine => {
-									self.state = State::MultLineComment(match c {
+									buf[index] = b'\\';
+									index += 1;
+									State::MultLineComment(match c {
 										b'\n' => MultiLineMark::Lf,
 										b'\r' => MultiLineMark::Cr,
 										_ => MultiLineMark::None,
-									});
-									buf[offset] = b'\\';
-									offset += 1;
+									}, false, Some(c))
 								}
 								MultiLineMark::Space => {
-									self.state = State::MultLineComment(match c {
+									State::MultLineComment(match c {
 										b'\n' => MultiLineMark::Lf,
 										b'\r' => MultiLineMark::Cr,
 										_ => MultiLineMark::None,
-									});
+									}, false, Some(c))
 								}
 								MultiLineMark::Lf if c == b'\n' => {
-									buf[offset] = b'\\';
-									offset += 1;
+									buf[index] = b'\\';
+									index += 1;
+									State::MultLineComment(multiline, false, Some(c))
 								}
 								MultiLineMark::Cr if c == b'\r' => {
-									buf[offset] = b'\\';
-									offset += 1;
+									buf[index] = b'\\';
+									index += 1;
+									State::MultLineComment(multiline, false, Some(c))
 								}
-							    _ => {							    	
+							    _ => {
+							    	State::MultLineComment(multiline, false, Some(c))
 							    }
 							}
-							buf[offset] = c;
-							offset += 1;
 						}
-						_ => {
-						}
+						_ => State::MultLineComment(multiline, false, None),
 					}
-				}
-				State::SingleLineComment(_) => {
 				}
 			}
 		}
-		if offset < buf.len() {
-			match self.state {
-				State::Code(_) | State::Quote(_) | State::Escape(_) => {
-					match self.last {
-						Some(c) if c == b'/' => {
-						}
-						Some(c) => {
-							buf[offset] = c;
-							offset += 1;
-							self.last = None;
-						}
-						None => {
-						}
-					}
-				}
-				State::SingleLineComment(_) | State::MultLineComment(_) => {
-				}
-			}
+		if index < buf.len() {
+			index = self.flush(buf, index);
 		}
-		if size == 0 {
-			return Ok(offset);
-		}
-		match offset {
-			0 => self.read(buf),
-			_ => Ok(offset),
-		}
+		return Ok(index);
 	}
 }
 
@@ -213,7 +228,7 @@ mod test {
 		let mut stream: Vec<u8> = Vec::new();
 		stream.write(original.as_bytes()).unwrap();
 
-		let mut filter = CommentsRemover::new(Cursor::new(stream));
+		let mut filter = CommentsRemover::new_with_buffer(Cursor::new(stream), block_size);
 		let mut actual = Vec::new();
 		let mut buffer = vec![0; block_size];
 		loop {
@@ -222,7 +237,7 @@ mod test {
 				break;
 			}
 			actual.write(&buffer[0..size]).unwrap();
-			assert!(actual.len() <= expected.len());
+			assert!(actual.len() <= expected.len() * 2);
 		}
 		assert_eq!(expected, String::from_utf8(actual).unwrap());
 	}
@@ -231,6 +246,7 @@ mod test {
 		check_filter_pass(original, expected, expected.len());
 		check_filter_pass(original, expected, original.len());
 		check_filter_pass(original, expected, 1);
+		check_filter_pass(original, expected, 3);
 		check_filter_pass(original, expected, 10);
 		check_filter_pass(original, expected, expected.len() - 1);
 		check_filter_pass(original, expected, original.len() - 1);
@@ -296,6 +312,8 @@ Bar */ \
           "B2"
 
 int main() {
+	/** Foo
+	 */
     return 0;
 }
 "#,
@@ -309,6 +327,8 @@ r#"#define A "A1" \
           "B2"
 
 int main() {
+	
+
     return 0;
 }
 "#
