65867d9bd8656233810935b051a2a423b16d0036
Artem V. Navrotskiy <bozaro@yandex.ru>
2015-12-01 23:16:48 +0300

    Add choco .nuspec

---
+++ choco/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = "Octobuild"
+$fileType = "msi"
+$silentArgs = "/quiet ADDLOCAL=ALL"
+$url   = "http://dist.bozaro.ru/windows/octobuild-X.X.X-i686.msi"
+$url64 = "http://dist.bozaro.ru/windows/octobuild-X.X.X-x86_64.msi"
+
+Install-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$url" "$url64"
