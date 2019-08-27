0315f7162c84a07db2741c198fcc004b136156eb
Marat Radchenko <marat@slonopotamus.org>
2019-08-27 10:34:43 +0300

    Compiler warning fix

--- src/vs/compiler.rs
+++ src/vs/compiler.rs
@@ -72,7 +72,7 @@ impl Compiler for VsCompiler {
     }
 
     #[cfg(unix)]
-    fn discovery_toolchains(&self) -> Vec<Arc<Toolchain>> {
+    fn discovery_toolchains(&self) -> Vec<Arc<dyn Toolchain>> {
         Vec::new()
     }
 
