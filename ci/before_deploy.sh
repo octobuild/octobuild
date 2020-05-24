0c1ad6028a451d23026d749e56a5076e6bfc7e65
Marat Radchenko <marat@slonopotamus.org>
2020-05-24 22:32:59 +0300

    Move from Travis CI to GitHub Actions

--- ci/before_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ex
-
-if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
-    sed -i "s/__SUBJECT__/${BINTRAY_USER}/g" bintray-descriptor.json
-    sed -i "s/__REPO_SLUG__/${TRAVIS_REPO_SLUG//\//\\/}/g" bintray-descriptor.json
-    sed -i "s/__VERSION__/${TRAVIS_TAG}/g" bintray-descriptor.json
-fi
