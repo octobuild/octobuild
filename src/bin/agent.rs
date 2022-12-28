70441708f43117cc5df94e6075952080f4e52d50
Marat Radchenko <marat@slonopotamus.org>
2022-12-28 13:36:27 +0300

    Add dummy agent service to make Unreal Engine happy

---
+++ src/bin/agent.rs
@@ -0,0 +1,17 @@
+use daemon::Daemon;
+use daemon::DaemonRunner;
+use daemon::State;
+use std::sync::mpsc::Receiver;
+
+fn main() {
+    let daemon = Daemon {
+        name: "octobuild_agent".to_string(),
+    };
+
+    daemon
+        .run(move |rx: Receiver<State>| {
+            // Just consume all signals until channel is closed
+            rx.iter().for_each(drop);
+        })
+        .unwrap();
+}
