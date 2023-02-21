octobuild/octobuild

commit 3c0b24f0508806b6aaa1086828b3e5808d469811

Silence "Can't use octobuild for task XXX: toolchain not found" logspam

src/worker.rs
@@ -11,6 +11,7 @@ use crate::compiler::{
     BuildTaskResult, CommandArgs, CommandInfo, CompilationTask, Compiler, OutputInfo, SharedState,
     Toolchain,
 };
+use crate::Error;
 
 pub type BuildGraph = Graph<Arc<BuildTask>, ()>;
 
@@ -103,7 +104,7 @@ impl BuildAction {
         title: &str,
         run_second_cpp: bool,
     ) -> Vec<BuildAction> {
-        let actions: Vec<BuildAction> = compiler
+        compiler
             .create_tasks(command.clone(), args.clone(), run_second_cpp)
             .map(|tasks| {
                 tasks
@@ -112,13 +113,14 @@ impl BuildAction {
                     .collect()
             })
             .unwrap_or_else(|e| {
-                println!("Can't use octobuild for task {title}: {e}");
-                Vec::new()
-            });
-        if actions.is_empty() {
-            return vec![BuildAction::Exec(command, args)];
-        }
-        actions
+                match e {
+                    Error::ToolchainNotFound(_) => {}
+                    e => {
+                        println!("Can't use octobuild for task {title}: {e}");
+                    }
+                }
+                vec![BuildAction::Exec(command, args)]
+            })
     }
 
     #[must_use]

commit 6b713b7c41206efdc16a100c178525fdde461578

Fix compilation of `.gch` files using Clang
Broken by 3c0b24f0508806b6aaa1086828b3e5808d469811

src/worker.rs
@@ -11,7 +11,6 @@ use crate::compiler::{
     BuildTaskResult, CommandArgs, CommandInfo, CompilationTask, Compiler, OutputInfo, SharedState,
     Toolchain,
 };
-use crate::Error;
 
 pub type BuildGraph = Graph<Arc<BuildTask>, ()>;
 
@@ -104,7 +103,7 @@ impl BuildAction {
         title: &str,
         run_second_cpp: bool,
     ) -> Vec<BuildAction> {
-        compiler
+        let actions: Vec<BuildAction> = compiler
             .create_tasks(command.clone(), args.clone(), run_second_cpp)
             .map(|tasks| {
                 tasks
@@ -114,13 +113,17 @@ impl BuildAction {
             })
             .unwrap_or_else(|e| {
                 match e {
-                    Error::ToolchainNotFound(_) => {}
+                    crate::Error::ToolchainNotFound(_) => {}
                     e => {
                         println!("Can't use octobuild for task {title}: {e}");
                     }
                 }
-                vec![BuildAction::Exec(command, args)]
-            })
+                Vec::new()
+            });
+        if actions.is_empty() {
+            return vec![BuildAction::Exec(command, args)];
+        }
+        actions
     }
 
     #[must_use]